b515b411af1fc1240f45196217225e63767251c7
henriupton99 <72378839+henriupton99@users.noreply.github.com>
2023-02-22 20:47:37 +0100

    BLEU metric update

--- code/BLEU.py
+++ code/BLEU.py
@@ -2,12 +2,12 @@ import numpy as np
 
 class Sentence_BLEU:
     
-    def __init__(self, tokenizer):
+    def __init__(self, N):
         
-        self.tok = tokenizer
+        self.N = N
+        self.w = np.ones(N)/N
     
-    @staticmethod
-    def compute_n_grams(sentence, n):
+    def compute_n_grams(self, sentence, n):
         
         sentence = sentence.lower()
         
@@ -23,7 +23,49 @@ class Sentence_BLEU:
             
             grams.append(" ".join(sentence[l : l + n]))
             
-        return grams
+        Grams, Counts = np.unique(grams, return_counts = True)
+        
+        return Grams, Counts
+    
+    def modified_precision(self, reference, candidate, n):
+        
+        G_ref, C_ref = self.compute_n_grams(reference, n)
+        
+        G_hyp, C_hyp = self.compute_n_grams(candidate, n)
+        
+        dict_ref = {G_ref[k] : C_ref[k] for k in range(len(G_ref)) }
+        dict_hyp = {G_hyp[k] : C_hyp[k] for k in range(len(G_hyp)) }
+        
+        p_n = 0
+        
+        for gram, count in dict_hyp.items():
+            
+            if gram not in G_ref :
+                
+                pass
+            
+            else:
+                
+                p_n += min(dict_ref[gram], count) / count
+            
+        return p_n
+    
+    def compute_score(self, reference, candidate):
+        
+        P_n = np.empty(self.N)
+        
+        for n in range(1, self.N+1):
+            
+            p_n = self.modified_precision(reference, candidate, n)
+            
+            P_n[n-1] = p_n
+        
+        
+        return np.exp(np.sum(self.w * np.log(P_n)))
+    
+        
+        
+
             
         
         
